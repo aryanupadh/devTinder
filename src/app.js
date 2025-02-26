5fed841a6bfbee2c7d4b2a65e6497c1a6c0032f4
Aryan Upadhyay <aryanupadh.developer@gmail.com>
2025-02-26 13:16:53 +0530

    routing changes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 const express = require("express")
 
 const app = express()
+let session = null
 
-app.use('/home', (req,res)=>{
-    res.send("HOME SCREEEN")
+app.use((req, res, next)=>{
+    console.log("entered middleware \-.-/")
+    session = true
+    next()
 })
 
-app.use('/about', (req,res)=>{
-    res.send("ABOUTTT")
+app.get('/about', (req,res)=>{
+    res.send("ABOUT")
+    console.log("Status About - "+session)
 })
 
-app.use('/', (req,res)=>{
-    res.send("It worksss.......")
+app.get('/home', (req,res)=>{
+    res.send("HOME SWEET HOME...")
+    console.log("Status Home - "+session)
 })
 
-app.listen(3000,()=>{
-    console.log("Server is listening")
+app.listen(3000, ()=>{
+    console.log("SERVER IS LISTENING ::: ")
 })
